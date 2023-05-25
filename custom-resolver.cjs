d381e0991be37f4b52e109be84bdb879664abd6b
Jack Tanner <jack@jackandtheblockstalk.com>
2023-05-25 19:03:13 +0200

    docs: better documentation

--- custom-resolver.cjs
+++ custom-resolver.cjs
@@ -4,6 +4,8 @@ module.exports = (path, options) => {
         ...options,
         // Use packageFilter to process parsed `package.json` before the resolution (see https://www.npmjs.com/package/resolve#resolveid-opts-cb)
         packageFilter: (pkg) => {
+            // Not sure why this is needed, as uint8arrays and multiformats are both successfully resolving in did-jwt with the same testEnvironment...
+            // https://github.com/decentralized-identity/did-jwt/blob/master/jest.config.mjs
             if (pkg.name === 'uint8arrays' || pkg.name.startsWith('multiformats')) {
                 const exports = pkg.exports;
                 const newExports = {};
@@ -11,6 +13,8 @@ module.exports = (path, options) => {
                 for (const key in exports) {
                     newExports[key] = exports[key];
                     // uint8arrays and multiformats only have an "import" export, which makes them fail during jest resolver
+                    // "import" is not a default condition when resolving using https://www.npmjs.com/package/resolve.exports
+                    // (used by jest resolver) for "node" testEnvironment
                     if (!exports[key]['node']) newExports[key]['node'] = exports[key]['import'];
                 }
 
