5e3160f7c6b9fd4df4e95d5ac59fb73ac77f81e4
Jack Tanner <jack@jackandtheblockstalk.com>
2023-06-04 21:05:57 +0200

    test: fixed test by removing asn1.js and using bn.js directly

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,13 +1,11 @@
 import { bnToBase64Url } from '../src/utils';
 import BN from 'bn.js';
 
-const bignum = require('asn1.js').bignum;
-
 // copied from https://github.com/Brightspace/node-jwk-to-pem/blob/master/src/b64-to-bn.js
 // to compare against this implementation
 function base64ToBigNum(val: string) {
   const buf = Buffer.from(val, 'base64');
-  const bn = (val = new bignum(buf, 10, 'be').iabs());
+  const bn = new BN(buf, 10, 'be').iabs();
 
   return bn;
 }
