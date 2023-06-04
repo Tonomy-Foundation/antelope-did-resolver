fd63658c1183c914144bc472aed75f2502ccb1d5
Jack Tanner <jack@jackandtheblockstalk.com>
2023-06-04 21:43:57 +0200

    test: removed unnecessary jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,14 +12,8 @@ const config = {
             },
         ],
     },
-    // // typescript 5 removes the need to specify relative imports as .js, so we should no longer need this workaround
-    // // but webpack still requires .js specifiers, so we are keeping it for now
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
     extensionsToTreatAsEsm: ['.ts'],
     testMatch: ['**/*.test.ts'],
-    coverageProvider: 'v8',
 };
 
 export default config;
